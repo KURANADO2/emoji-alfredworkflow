615778c75fce3c8b9a1619fe380a77ba73d87760
kuranado <jingxinling1024@163.com>
2020-06-27 17:59:06 +0800

    feat: 使用多线程下载异步加速下载图片列表,提升用户体验
    对于首次搜索到的图片 url 列表,因为本地没有缓存文件,需要执行下载动作,但单线程下载速度缓慢
    一般 9 张表情包大小合计约为 500 KB左右,单线程需要 5 ~ 6 秒完成下载,而异步多线程可在 1s 内完成,显著提升用户搜索体验

--- emoji.py
+++ emoji.py
@@ -3,6 +3,7 @@
 
 import sys
 from workflow import Workflow3, web
+from multiprocessing.pool import ThreadPool
 import urllib2
 import os
 
@@ -10,7 +11,7 @@ ICON_DEFAULT = 'icon.png'
 headers = {"Referer": "http://kuranado.com"}
 
 
-def list(query=None):
+def list_emoji(query=None):
     url = "http://api.kuranado.com/emoji/list"
     params = dict(keyword=query, page=1, size=9)
 
@@ -22,27 +23,37 @@ def list(query=None):
     emojis = []
     data = r.json()
 
+    path = "/tmp/emoji/"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    pool = ThreadPool(processes=4)
+
     for d in data['data']:
 
         image_name = d['url'][d['url'].index('emoji') + 6:]
-        path = "/tmp/emoji/"
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        file_name = path + image_name
-        d['path'] = file_name
+        key_name = path + image_name
+        d['path'] = key_name
         emojis.append(d)
 
-        if os.path.exists(file_name):
+        if os.path.exists(key_name):
             continue
 
-        request = urllib2.Request(d['url'], headers=headers)
-        response = urllib2.urlopen(request)
-        if response.getcode() == 200:
-            with open(file_name, "wb") as f:
-                f.write(response.read())
-
+        pool.apply_async(func=download, args=(d['url'], key_name))
+    pool.close()
+    pool.join()
     return emojis
+
+
+def download(url, out_dir):
+
+    request = urllib2.Request(url, headers=headers)
+    response = urllib2.urlopen(request)
+    if response.getcode() == 200:
+        with open(out_dir, "wb") as f:
+            f.write(response.read())
+    else:
+        logger.debug("图片下载失败")
 
 
 def main(wf):
@@ -55,7 +66,7 @@ def main(wf):
     #     return list(query)
     #
     # emojis = wf.cached_data('list', wrapper, max_age=600)
-    emojis = list(query)
+    emojis = list_emoji(query)
 
     if len(emojis) <= 0:
         wf.add_item(title=u'未找到表情包', valid=True, icon=ICON_DEFAULT)
